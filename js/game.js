95fefe4c8a2bf5a5cb29daa3377528a8acb4201b
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2015-11-18 08:36:33 -0800

    move feedback display flex from being set in JS to css.css.

--- js/game.js
+++ js/game.js
@@ -147,13 +147,9 @@ function spotOneClick () {
       score = speedFactor * shuffles;
       isHighScore(score);
       popup.setAttribute('class', 'popup');
-      feedback.style.display = 'flex';
-      feedback.style.color = 'green';
       feedback.innerHTML = '<a href="scores.html" class="win">You win! <br />Your score is ' + score + '.<br />Click to see high scores.</a>';
       } else {
         popup.setAttribute('class', 'popup');
-        feedback.style.display = 'flex';
-        feedback.style.color = 'red';
         feedback.innerHTML = '<a href="scores.html" class="lose">You lose! <br />Your score is ' + score + '.<br />Click to see high scores.</a>';
     } //produce feedback
   }
@@ -167,14 +163,10 @@ function spotTwoClick () {
     if(spotTwo.children[0].children[0].id === 'winner'){
       score = speedFactor * shuffles;
       popup.setAttribute('class', 'popup');
-      feedback.style.display = 'flex';
-      feedback.style.color = 'green';
       feedback.innerHTML = '<a href="scores.html" class="win">You win! <br />Your score is ' + score + '.<br />Click to see high scores.</a>';
       isHighScore(score);
     } else {
       popup.setAttribute('class', 'popup');
-      feedback.style.display = 'flex';
-      feedback.style.color = 'red';
       feedback.innerHTML = '<a href="scores.html" class="lose">You lose! <br />Your score is ' + score + '.<br />Click to see high scores.</a>';
     } //produce feedback
   }
@@ -188,14 +180,10 @@ function spotThreeClick () {
     if(spotThree.children[0].children[0].id === 'winner'){
       score = speedFactor * shuffles;
       popup.setAttribute('class', 'popup');
-      feedback.style.display = 'flex';
-      feedback.style.color = 'green';
       feedback.innerHTML = '<a href="scores.html" class="win">You win! <br />Your score is ' + score + '.<br />Click to see high scores.</a>';
       isHighScore(score);
     } else {
       popup.setAttribute('class', 'popup');
-      feedback.style.display = 'flex';
-      feedback.style.color = 'red';
       feedback.innerHTML = '<a href="scores.html" class="lose">You lose! <br />Your score is ' + score + '.<br />Click to see high scores.</a>';
     } //produce feedback
   }
